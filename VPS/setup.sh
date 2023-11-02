f6b4559364ab7715a32c63ab6cdf6506a4e8cbe5
dukes <124644184+yuzadef@users.noreply.github.com>
2023-11-02 11:10:49 +0800

    Update setup.sh

--- VPS/setup.sh
+++ VPS/setup.sh
@@ -1,15 +1,5 @@
-#!/bin/bash
-# script to setup hunting tools
-
-if [ "$EUID" -ne 0 ]
-    then echo "Run as root!"
-    exit
-fi
-
-cd ~/
-
-add-apt-repository universe -y
-apt update && apt upgrade -y
+sudo add-apt-repository universe -y
+sudo apt update && apt upgrade -y
 
 programs=(
 curl
@@ -40,22 +30,20 @@ nikto
 wfuzz
 masscan
 hydra
-cewl
 whois
 )
 
-# install each tools using APT
 echo "[+] Installing APT programs"
 for tool in "${programs[@]}"; do
-       apt install "$tool" -y
+       sudo apt install "$tool" -y
 done
 echo "[+] Done"
 
 # install go, manually update to latest version
 echo "[+] Installing GO"
 wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
-ln -s /usr/local/go/bin/go /usr/local/bin/go
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
+sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 echo "[+] Done"
 
 go_programs=(
@@ -77,87 +65,59 @@ github.com/BishopFox/jsluice/cmd/jsluice@latest
 # install go programs
 echo "[+] Installing GO programs"
 for url in "${go_programs[@]}"; do
-	go install -v "$url"
+        go install -v "$url"
 done
-echo "[+] Done"
 
 # symlink go programs
 echo "[+] Symlinking GO programs"
-ln -s ~/go/bin/jsluice /usr/local/bin/jsluice
-ln -s ~/go/bin/amass /usr/local/bin/amass
-ln -s ~/go/bin/ffuf /usr/local/bin/ffuf
-ln -s ~/go/bin/assetfinder /usr/local/bin/assetfinder
-ln -s ~/go/bin/gau /usr/local/bin/gau
-ln -s ~/go/bin/gf /usr/local/bin/gf
-ln -s ~/go/bin/gobuster /usr/local/bin/gobuster
-ln -s ~/go/bin/hakrawler /usr/local/bin/hakrawler
-ln -s ~/go/bin/httprobe /usr/local/bin/httprobe
-ln -s ~/go/bin/subfinder /usr/local/bin/subfinder
-ln -s ~/go/bin/waybackurls /usr/local/bin/waybackurls
-ln -s ~/go/bin/nuclei /usr/local/bin/nuclei
-ln -s ~/go/bin/httpx /usr/local/bin/httpx
-ln -s /usr/bin/python3 /usr/bin/python
+sudo ln -s ~/go/bin/jsluice /usr/local/bin/jsluice
+sudo ln -s ~/go/bin/amass /usr/local/bin/amass
+sudo ln -s ~/go/bin/ffuf /usr/local/bin/ffuf
+sudo ln -s ~/go/bin/assetfinder /usr/local/bin/assetfinder
+sudo ln -s ~/go/bin/gau /usr/local/bin/gau
+sudo ln -s ~/go/bin/gf /usr/local/bin/gf
+sudo ln -s ~/go/bin/gobuster /usr/local/bin/gobuster
+sudo ln -s ~/go/bin/hakrawler /usr/local/bin/hakrawler
+sudo ln -s ~/go/bin/httprobe /usr/local/bin/httprobe
+sudo ln -s ~/go/bin/subfinder /usr/local/bin/subfinder
+sudo ln -s ~/go/bin/waybackurls /usr/local/bin/waybackurls
+sudo ln -s ~/go/bin/nuclei /usr/local/bin/nuclei
+sudo ln -s ~/go/bin/httpx /usr/local/bin/httpx
+sudo ln -s /usr/bin/python3 /usr/bin/python
 echo "[+] Done"
 
-# installing Sqlmap
-echo "[+] Installing Sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git /opt/sqlmap
-echo 'alias sqlmap="python3 /opt/sqlmap-dev/sqlmap.py"' >> ~/.bashrc
-source ~/.bashrc
-echo "[+] Done"
+# installing sqlmap
+pip3 install sqlmap
+sudo ln -s ~/.local/bin/sqlmap /usr/local/bin/sqlmap
+
+# installing seclists
+sudo git clone https://github.com/danielmiessler/SecLists.git /opt/seclists
+
+# install arjun
+pip3 install arjun
+sudo ln -s ~/.local/bin/sqlmap /usr/local/bin/sqlmap
+
+# install wpscan
+sudo gem install wpscan
 
 # installing Ghauri
 echo "[+] Installing Ghauri"
-git clone https://github.com/r0oth3x49/ghauri.git /opt/ghauri
-python3 /opt/ghauri/setup.py install
+sudo git clone https://github.com/r0oth3x49/ghauri.git /opt/ghauri
+pip3 install -r requirements.txt
+sudo python3 /opt/ghauri/setup.py install
 echo "[+] Done"
 
 # installing Paramspider
 echo "[+] Installing Paramspider"
-git clone https://github.com/devanshbatham/ParamSpider.git /opt/paramspider
-python3 /opt/Paramspider/setup.py install
-echo "[+] Done"
-
-# installing dirsearch
-echo "[+] Installing dirsearch"
-git clone https://github.com/maurosoria/dirsearch.git /opt/dirsearch
-python3 /opt/dirsearch/setup.py install
-echo "[+] Done"
-
-# installing seclists
-echo "[+] Installing SecLists"
-git clone https://github.com/danielmiessler/SecLists.git /opt/seclists
-echo "[+] Done"
-
-# installing metasploit-framework
-echo "[+] Installing metasploit"
-mkdir /opt/metasploit-framework
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /opt/metasploit-framework/msfinstall
-chmod 755 /opt/msfinstall
-/opt/msfinstall
-echo "[+] Done"
-
-# installing Arjun
-echo "[+] Installing Arjun"
-pip3 install arjun
-echo "[+] Done"
-
-# install WpScan
-echo "[+] Installing WpScan"
-gem install wpscan
+sudo git clone https://github.com/devanshbatham/ParamSpider.git /opt/paramspider
+sudo python3 /opt/Paramspider/setup.py install
 echo "[+] Done"
 
 # post installing GF
 echo "[+] Post install GF"
-git clone https://github.com/tomnomnom/gf.git /usr/local/go/src/
+sudo git clone https://github.com/tomnomnom/gf.git /usr/local/go/src/
 echo 'source /usr/local/go/src/gf/gf-completion.bash' >> ~/.bashrc
-git clone https://github.com/emadshanab/Gf-Patterns-Collection.git /usr/local/go/src/gf/
-chmod +x /usr/local/go/src/gf/Gf-Patterns-Collection/set-all.sh && /usr/local/go/src/gf/Gf-Patterns-Collection/set-all.sh
-echo "[+] Done"
-
-# post installing python3-impacket
-echo "[+] Post install python3-impacket"
-cp /usr/share/doc/python3-impacket/examples/* /usr/bin/
+sudo cp /usr/local/go/src/gf/examples/* ~/.gf
 echo "[+] Done"
 
 # setup tmux configuration file
@@ -180,17 +140,4 @@ bind -n M-Down select-pane -D
 bind -n M-Left select-pane -L
 bind -n M-Right select-pane -R
 """ > ~/.tmux.conf
-echo "[+] Done"
-
-# update ~/.bash_profile
-echo "[+] Updating bash_profile"
-echo """
-if [ -f ~/.bashrc ]; then
-    source ~/.bashrc
-fi
-
-if [ -f ~/.tmux.conf ]; then
-    tmux source-file ~/.tmux.conf
-fi
-""" >> ~/.bash_profile
 echo "[+] Done"
